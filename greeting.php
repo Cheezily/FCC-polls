80f01d2383ce5b15f19bb5759d97145a7de140b4
Cheezily <pm655@yahoo.com>
2016-07-31 11:47:46 -0500

    remove most html from index.php so it can act more as a controller

---
+++ greeting.php
@@ -0,0 +1,99 @@
+    <?php if(isset($login)) { ?>
+        <div class="container hideContainer">
+    <?php } else { ?>
+        <div class="container">
+    <?php } ?>
+            
+        <div class='greeting'>
+          <div class='leftSideStuff'>
+            <h2>Already a registered user?</h2>
+            <form method="get" action="">
+                <input type="submit" value="Login" name="login">
+            </form>
+            <form method="get" action="">
+                <input type="submit" value="New User" name="newUser">
+            </form>
+          </div>
+
+          <div class="itemsLabel">Recent Polls -- Click to Participate</div>
+          <div class='items'>
+
+            <div class='item'>
+              <h1>Box #12</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #11</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #10</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #9</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #8</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #7</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #6</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #5</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #4</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #3</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #2</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #1</h1>
+            </div>
+            <!--repeat box 1 through 6-->
+            <div class='item'>
+              <h1>Box #12</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #11</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #10</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #9</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #8</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #7</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #6</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #5</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #4</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #3</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #2</h1>
+            </div>
+            <div class='item'>
+              <h1>Box #1</h1>
+            </div>
+          </div>
+            <div class="shadowLayer">
+
+            </div>
+        </div>
+    </div>
---
+++ login.php
@@ -0,0 +1,9 @@
+<div class="loginGreeting">
+    <form method="post" action="">
+        <label for='loginName'>Username</label>
+        <input type='text' name='username'>
+        <label for='loginPW'>Password</label>
+        <input type='password' name='password'>
+        <input type='submit' value='Login'>
+    </form>
+</div>
