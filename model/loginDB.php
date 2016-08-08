ffa154bd57cf064d65e5b1002e26cbc3d272fbae
Cheezily <pm655@yahoo.com>
2016-08-08 12:30:04 -0500

    add login and account creation timestamp

--- model/loginDB.php
+++ model/loginDB.php
@@ -13,6 +13,11 @@ function checkUsernamePW($usernameCheck, $pwCheck) {
     $userInfo = $statement->fetch();
     
     if (password_verify($pwCheck, $userInfo['passwordHash'])) {
+        $query = "UPDATE users SET lastActivity=:lastActivity WHERE userID=:userID";
+        $statement = $db->prepare($query);
+        $statement->bindValue(":userID", $userInfo['userId']);
+        $statement->bindValue(":lastActivity", date("Y-m-d H:i:s"));
+        $statement->execute();
         return $userInfo;
     } else {
         return FALSE;
@@ -32,13 +37,14 @@ function checkForUsername($usernameCheck) {
 
 function saveNewUser($usernameCheck, $password) {
     global $db;
-    $query = "INSERT INTO users(username, passwordHash) VALUES (:usernameCheck, :password)";
+    $query = "INSERT INTO users(username, passwordHash, created) VALUES (:usernameCheck, :password, :created)";
     
     $hashOptions = ['cost' => 11];
     $passwordHash = password_hash($password, PASSWORD_BCRYPT, $hashOptions);
     $statement = $db->prepare($query);
     $statement->bindValue(":usernameCheck", $usernameCheck);
     $statement->bindValue(":password", $passwordHash);
+    $statement->bindValue(":created", date("Y-m-d H:i:s"));
     $statement->execute();
     
     return checkUsernamePW($usernameCheck, $password);
